11712018837834a342ac771b90d21f9c1ffb59e8
GogaIV <jauhargoga@gmail.com>
2024-07-28 20:17:23 +1200

    Worked on home
    Will do more tn this is a update point

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,31 @@
-/*=============== SHOW MENU ===============*/
+// Creating Menu identity in JS
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+// Menu Show on click
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
 
-/*=============== REMOVE MENU MOBILE ===============*/
+// Menu hide on click of X
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
+// Mobile Menu Removal (Responsive)
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
